bd126d743151b6411513a38139caf04abc978ba8
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-10-09 05:54:14 +0900

    Initial Setup for foodie project

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import reportWebVitals from "./reportWebVitals";
+import App from "./app/App";
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+const container = document.getElementById("root")!; //-> real DOM
+const root = createRoot(container); //-> virtual DOM
 
 root.render(
   <React.StrictMode>
