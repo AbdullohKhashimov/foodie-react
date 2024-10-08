f23a6d5d1fe05868fb9adee15d2836aca40faa3b
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-10-09 06:14:14 +0900

    feat: MUI standard integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "../css/app.css";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Foodie</h1>
-    </div>
+    <Container maxWidth="sm">
+      <Stack flexDirection={"column"}>
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h1" component={"h1"}>
+            Create React App on Foodie
+          </Typography>
+          <Button variant={"outlined"}>Success</Button>
+        </Box>
+      </Stack>
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import reportWebVitals from "./reportWebVitals";
 import App from "./app/App";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme";
 
 const container = document.getElementById("root")!; //-> real DOM
 const root = createRoot(container); //-> virtual DOM
@@ -11,7 +14,10 @@ const root = createRoot(container); //-> virtual DOM
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
